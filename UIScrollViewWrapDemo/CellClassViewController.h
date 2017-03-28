c623181f570b74ee49d79170367bfb590658762f
Silence <374619540@qq.com>
2017-03-28 14:24:48 +0800

    Update

--- UIScrollViewWrapDemo/CellClassViewController.h
+++ UIScrollViewWrapDemo/CellClassViewController.h
@@ -2,7 +2,7 @@
 //  CellClassViewController.h
 //  UIScrollViewWrapDemo
 //
-//  Created by 杨晴贺 on 2017/3/28.
+//  Created by Silence on 2017/3/28.
 //  Copyright © 2017年 Silence. All rights reserved.
 //
 
--- UIScrollViewWrapDemo/GroupViewController.h
+++ UIScrollViewWrapDemo/GroupViewController.h
@@ -2,7 +2,7 @@
 //  GroupViewController.h
 //  UIScrollViewWrapDemo
 //
-//  Created by 杨晴贺 on 2017/3/28.
+//  Created by Silence on 2017/3/28.
 //  Copyright © 2017年 Silence. All rights reserved.
 //
 
